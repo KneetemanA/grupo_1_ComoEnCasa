6e6e1ad827ae2bd096e1a58206086e02c726fe25
agustin9700 <ryuk10xd@gmail.com>
2024-04-24 18:47:32 -0300

    iniciando spring 6

---
+++ database/migrations/20240424003303-create-info-user.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('infoUsers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      province: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      street: {
+        type: Sequelize.STRING
+      },
+      num: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('infoUsers');
+  }
+};
---
+++ database/models/infouser.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class infoUser extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  infoUser.init({
+    phone: DataTypes.STRING,
+    province: DataTypes.STRING,
+    city: DataTypes.STRING,
+    street: DataTypes.STRING,
+    num: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'infoUser',
+    timestamps:false
+  });
+  return infoUser;
+};
