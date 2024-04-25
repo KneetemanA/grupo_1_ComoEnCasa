e2e52d3cafe63ca7d1897bda2de4adc0ac557e1a
erbaLuka7 <ezequielerba7@gmail.com>
2024-04-25 17:58:11 -0300

    refactorizacion de creacion de producto realizada pero sin migraciones y rero realizada

--- controllers/admin/newProduct.controller.js
+++ controllers/admin/newProduct.controller.js
@@ -1,15 +1,21 @@
-const { loadData,saveData} = require("../../database");
-
-
-
+//const { loadData,saveData} = require("../../database");
+const db = require("../../database/models")
 module.exports = function(req, res) {
-    let products = loadData("productos")
+    //let products = loadData("productos")
     const imgInfo= req.file;
-    
     const { name,category,price,discount,freeShipping,detail } = req.body;
-   
-    const newid = products[products.length - 1].id + 1;
-    const newProduct = {
+    //const newid = products[products.length - 1].id + 1;
+    db.Product.create({
+        name:name.trim(),
+        price:+price,
+        discount:+discount,
+        free_shipping: freeShipping,
+        image: imgInfo? `/images/${imgInfo.filename}` : "/images/default.jpg",
+        detail: detail.trim(),
+        category_id: +category
+    })
+    .then((p=>{return res.redirect("/admin")}))
+    /*const newProduct = {
         id: +newid,
         category,
         name,
@@ -18,17 +24,7 @@ module.exports = function(req, res) {
         freeShipping: freeShipping === "true",
         image: imgInfo? `/images/${imgInfo.filename}` : "/images/default.jpg",
         detail
-    }
-    
-
-    products = [...products, newProduct];
-
-    saveData(products,"productos")
-
-    
-    
-    
-  
-    
-    return res.redirect("/admin");
+    }*/
+    //products = [...products, newProduct];
+    //saveData(products,"productos")
 }
