08467d12b19b408149c8cd84ccaa8bd3b9f1e24e
agustin9700 <ryuk10xd@gmail.com>
2024-04-25 00:32:24 -0300

    refactorizando vistas y consumiendo datos desde sql

--- controllers/home/home.controller.js
+++ controllers/home/home.controller.js
@@ -1,13 +1,14 @@
-const { loadData } = require("../../database")
+const { loadData } = require("../../database");
+const db = require("../../database/models");
 
-
-module.exports= (req,res) => {
-    const datos= loadData('productos')
-    const datosHamb = datos.filter(p => p.category === "Hamburguesas")
-
-    const userlogueado = req.session.user
-        
-    res.render('home', {datosProductos: datos, datosHamb, userlogueado})
-
-
-}
+module.exports = (req, res) => {
+  const datos= loadData('productos')
+  const datosHamb = datos.filter(p => p.category === "Hamburguesas") //PARA FILTRAR AUN SE SIGUE USANDO EL JSON, FALTA MIGRACIONES
+  const userlogueado = req.session.user;
+  db.Product.findAll({
+    limit:12
+  })
+  .then((products) => {
+    res.render("home", { datosProductos: products,datosHamb, userlogueado });
+  });
+};
