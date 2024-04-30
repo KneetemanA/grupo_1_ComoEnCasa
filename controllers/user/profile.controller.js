5d5db38e468a1f3185ffbce743daa2d629c02692
erbaLuka7 <ezequielerba7@gmail.com>
2024-04-30 02:51:06 -0300

    edit profile

--- controllers/user/profile.controller.js
+++ controllers/user/profile.controller.js
@@ -1,16 +1,19 @@
 const db = require("../../database/models")
 
 module.exports = (req, res) => {
-  const userlogueado = req.session.user;
-  db.infoUser
-    .findOne({
-      where: { user_id: userlogueado.id },
-    })
-    .then((infouser) => {
-      res.render("profileUser", { userlogueado, infouser });
-      console.log(infouser)
-    })
-    .catch((err) => {
-      res.send(err.message);
-    });
+  //const userlogueado = req.session.user;
+  db.User.findByPk(req.params.id)
+  .then((userlogueado=>{
+    db.infoUser
+      .findOne({
+        where: { user_id: userlogueado.id },
+      })
+      .then((infouser) => {
+        res.render("profileUser", { userlogueado, infouser });
+      })
+      .catch((err) => {
+        res.send(err.message);
+      });
+  }))
 };
+
