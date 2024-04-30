f4d2a59cb4bcce64b0c36b1bbf084996ec366671
agustin9700 <ryuk10xd@gmail.com>
2024-04-29 22:47:26 -0300

    arreglando slider en home

--- controllers/home/home.controller.js
+++ controllers/home/home.controller.js
@@ -1,14 +1,17 @@
-const { loadData } = require("../../database");
+
 const db = require("../../database/models");
 
 module.exports = (req, res) => {
-  const datos= loadData('productos')
-  const datosHamb = datos.filter(p => p.category === "Hamburguesas") //PARA FILTRAR AUN SE SIGUE USANDO EL JSON, FALTA MIGRACIONES
+  
+  
+  
   const userlogueado = req.session.user;
-  db.Product.findAll({
-    limit:12
-  })
+
+  db.Product.findAll()
   .then((products) => {
-    res.render("home", { datosProductos: products,datosHamb, userlogueado });
+   const datoProductos = products.slice(0, 12)
+    const datosHamb = products.filter(p => p.category_id === 3)
+    res.render("home", { datosProductos: datoProductos,datosHamb, userlogueado });
+    
   });
 };
