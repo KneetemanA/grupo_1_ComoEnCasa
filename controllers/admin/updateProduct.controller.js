0c0524e87d5d35939881a5c2694a90dc145a8d00
agustin9700 <ryuk10xd@gmail.com>
2024-05-02 18:55:28 -0300

    spring6 finalizado

--- controllers/admin/updateProduct.controller.js
+++ controllers/admin/updateProduct.controller.js
@@ -1,45 +1,46 @@
-const path = require('path')
-const fs = require('fs')
- const db = require('../../database/models')
- 
-module.exports = (req, res) => {
- 
+const path = require('path');
+const fs = require('fs');
+const db = require('../../database/models');
+
+module.exports = async (req, res) => {
   const { id } = req.params;
   const { category, name, price, discount, free_shipping, detail } = req.body;
   const image = req.file;
 
-  const images = db.Product.findOne({
-    where: { id : id}
-  })
-  .then((p) =>{
-    return p.image
-  })
-  
-  db.Product.update({
-    category_id: +category,
-    name: name ? name.trim() : name ,
-    price: +price,
-    discount: +discount ,
-    free_shipping: free_shipping === "true",
-    detail: detail ? detail.trim(): detail,
-    image: image ? `/images/${image.filename}` : "/images/default.jpg"  // req.files.image[0]?.filename
-  },{
-    where: {id}
-  })
-  .then((isUpdate)=>{
-    if(isUpdate.image?.filename){
-            const pathBefore = path.join(__dirname, `../../public${images}`);
-            const existsFile = fs.existsSync(pathBefore);
-      
-            if(existsFile){
-              fs.unlinkSync(pathBefore)
-             }
-          }
-    
-    res.redirect('/admin')
+  try {
+    const p = await db.Product.findByPk(id);
+    const imagenPrevia = p.image;
 
-  })
-}
+    await db.Product.update(
+      {
+        category_id: +category,
+        name: name ? name.trim() : name,
+        price: +price,
+        discount: +discount,
+        free_shipping: free_shipping === "true",
+        detail: detail ? detail.trim() : detail,
+        image: image ? `/images/${image.filename}` : "/images/default.jpg",
+      },
+      {
+        where: { id },
+      }
+    );
+
+    if (image && imagenPrevia !== "/images/default.jpg") {
+      const pathBefore = path.join(__dirname, `../../public${imagenPrevia}`);
+      const existsFile = fs.existsSync(pathBefore);
+
+      if (existsFile) {
+        fs.unlinkSync(pathBefore);
+      }
+    }
+
+    res.redirect('/admin');
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 // .then(image =>{
 //     if(image?.filename){
 //       const pathBefore = path.join(__dirname, `../../public${image}`);
