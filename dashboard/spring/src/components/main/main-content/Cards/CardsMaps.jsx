ee905691945c25561ef56f25a680f2e78d1a8956
erbaLuka7 <ezequielerba7@gmail.com>
2024-06-19 03:02:08 -0300

    tarjetas con todas las apis

--- dashboard/spring/src/components/main/main-content/Cards/CardsMaps.jsx
+++ dashboard/spring/src/components/main/main-content/Cards/CardsMaps.jsx
@@ -3,6 +3,7 @@ import Cards from "./index.jsx";
 const CardsMap = () => {
     const[productos,setProductos]=useState([])
     const[users,setUsers]=useState([])
+    const[categories,setCategories]=useState([])
     
      useEffect(() =>  {
       fetch("http://localhost:3030/api/list/products")
@@ -15,11 +16,17 @@ const CardsMap = () => {
           .then((res) => res.json())
           .then((data) => setUsers(data.data));
       }, []);
+
+      useEffect(() =>  {
+        fetch("http://localhost:3030/api/list/categories")
+          .then((res) => res.json())
+          .then((data) => setCategories(data.data));
+      }, []);
    
     return (
         <>
         <Cards title={"Productos"} number={productos.length} icon={"pizza-slice"}/>
-        <Cards title={"Categorias"} number={10} icon={"layer-group"}/>
+        <Cards title={"Categorias"} number={categories.length} icon={"layer-group"}/>
         <Cards title={"Usuarios"} number={users.length} icon={"users"}/>
         </>
     )
