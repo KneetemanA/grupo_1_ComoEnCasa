be5a4b6709f48b8a69b270d7bc841a941a7493e8
agustin9700 <ryuk10xd@gmail.com>
2024-06-23 03:09:57 -0300

    agregando vista ultimo user y product

--- dashboard/spring/src/components/main/main-content/MainContent.jsx
+++ dashboard/spring/src/components/main/main-content/MainContent.jsx
@@ -1,14 +1,14 @@
 import CardsMap from "./Cards/CardsMaps";
-import CardLastDate from "./CardsLastDate/CardLastDate";
+
 
 
 function MainContent() {
   return (
     <main className="col-md-10 mt-5 m-auto col-lg-10 p-4 text-white">
-      <div className="d-flex w-100 justify-content-between ">
-        <CardsMap/>
-      </div>
-      <CardLastDate/>
+     
+        < CardsMap/>
+      
+      
     </main>
   );
 }
